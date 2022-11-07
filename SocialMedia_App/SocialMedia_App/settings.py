3e475e82a604f3baa3f1e3d8745c31af441c8835
abhijeetsahu09 <abhijeetsahu937@gmail.com>
2022-11-07 22:28:44 +0530

    commit

--- SocialMedia_App/SocialMedia_App/settings.py
+++ SocialMedia_App/SocialMedia_App/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z)urk0+qtpax8*2*=ft@#%$l(z&rydcg&g&%e7ii+u_#@)5ges'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
